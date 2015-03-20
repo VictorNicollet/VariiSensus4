fefceb9d734ca52355845596849550eef913012e
Victor Nicollet <victor@nicollet.net>
2015-03-20 19:54:35 +0100

    Initial commit

---
+++ all.ml
@@ -0,0 +1,24 @@
+let all = [
+
+  (* Michel Onterberg découvre les principes du Varii Sensus. *)
+
+  (* Simmera vient exterminer Michel Onterberg, accompagnée des
+     enfants de la Brume. Elle garde son âme sous le coude en tant
+     qu'Archange. *)
+
+
+
+  (* Eugénie, Saïar et Nathan visitent ensemble une usine d'assemblage
+     de radios, à Nagovie. *)
+
+
+
+  (* Giselle propose son aide à Jeanne et Simmera, à Nymett. *)
+
+  (* Simmera rencontre Staniel, qui lui explique le principe de la 
+     passation et de nombreux détails historiques. *)
+  "molidaster", "Molidaster" ; 
+
+  
+
+]
