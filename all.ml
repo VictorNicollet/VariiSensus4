235f26c507d202c339e0539a127dec7476f366e1
Victor Nicollet <victor@nicollet.net>
2015-07-11 15:07:51 +0200

    Alliance

--- all.ml
+++ all.ml
@@ -12,9 +12,8 @@ let all = [
   (* Eugénie, Saïar et Nathan visitent ensemble une usine d'assemblage
      de radios, à Nagovie. *)
 
-
-
-  (* Giselle propose son aide à Jeanne et Simmera, à Nymett. *)
+  (* Giselle parle avec l'Empereur. *)
+  "alliance", "Alliance" ;
 
   (* Simmera rencontre Staniel, qui lui explique le principe de la 
      passation et de nombreux détails historiques. *)
